da10f261b7c933795ebdc6ac6976d4673fbeb2bd
vladisulian <106911452+vladisulian@users.noreply.github.com>
2023-02-27 15:51:11 +0000

    Deploying to gh-pages from @ vladisulian/goit-react-hw-08-phonebook@d530839f6eefa1b8d9793c61a676a1f8c2b124a6 🚀

---
+++ static/js/676.ec64993c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[676],{676:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var s=n(425),r=n(184),o=function(){return(0,r.jsxs)("div",{className:"notFoundPage",children:[(0,r.jsxs)("p",{children:[(0,r.jsx)(s.NLh,{}),"This page is not found or still not created..."]}),(0,r.jsxs)("p",{children:["You will be redirected to homepage in 5 seconds",(0,r.jsx)(s.mV1,{})]})]})}}}]);
+//# sourceMappingURL=676.ec64993c.chunk.js.map
