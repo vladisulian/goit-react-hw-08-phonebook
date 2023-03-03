5dd7809bd7bdc67e59716498570edefe0ac416a9
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-03-04 00:24:11 +0200

    Add ternar operator, 'if' render in contactInfo.jsx

--- src/Pages/Contacts/ContactsInfo/ContactInfo.jsx
+++ src/Pages/Contacts/ContactsInfo/ContactInfo.jsx
@@ -32,65 +32,67 @@ const ContactInfo = () => {
   const { id, name, surname, image, phone, country, city, job } = contactInfo;
   return (
     <>
-      <div className="contact-info">
-        <Link to={backLink} className="info__backlink">
-          <FiArrowLeftCircle />
-        </Link>
-        <div className="info">
-          <img src={image} alt="contact avatar" />
-          <p>
-            {name} {surname}
-          </p>
+      {contactInfo.hasOwnProperty('name') && (
+        <div className="contact-info">
+          <Link to={backLink} className="info__backlink">
+            <FiArrowLeftCircle />
+          </Link>
+          <div className="info">
+            <img src={image} alt="contact avatar" />
+            <p>
+              {name} {surname}
+            </p>
 
-          <Divider className="divider" />
+            <Divider className="divider" />
 
-          <div className="actions">
-            <Button>
-              <BsFillTelephoneFill className="actions__call" />
-              <p>Call</p>
-            </Button>
-            <Button>
-              <RiMessage2Fill className="actions__text" />
-              Text
-            </Button>
-            <Button>
-              <BsFillCameraVideoFill className="actions__video" />
-              Video
+            <div className="actions">
+              <Button>
+                <BsFillTelephoneFill className="actions__call" />
+                <p>Call</p>
+              </Button>
+              <Button>
+                <RiMessage2Fill className="actions__text" />
+                Text
+              </Button>
+              <Button>
+                <BsFillCameraVideoFill className="actions__video" />
+                Video
+              </Button>
+            </div>
+            <Divider className="divider" />
+
+            <div className="about">
+              <ul>
+                <li>
+                  <MdWork className="job-icon" />
+                  Job role: <span>{job}</span>
+                </li>
+                <li>
+                  <BsFillTelephoneFill className="phone-icon" /> Number:
+                  <span>{phone}</span>
+                </li>
+                <li>
+                  <GrMapLocation className="country-icon" /> Country:
+                  <span>{country}</span>
+                </li>
+                <li>
+                  <MdLocationCity className="city-icon" /> City:
+                  <span>{city}</span>
+                </li>
+              </ul>
+            </div>
+            <Button
+              colorScheme={'red'}
+              onClick={() => {
+                handleDelete(id);
+                navigate('/goit-react-hw-08-phonebook/contacts');
+              }}
+            >
+              Delete contact
             </Button>
           </div>
-          <Divider className="divider" />
-
-          <div className="about">
-            <ul>
-              <li>
-                <MdWork className="job-icon" />
-                Job role: <span>{job}</span>
-              </li>
-              <li>
-                <BsFillTelephoneFill className="phone-icon" /> Number:
-                <span>{phone}</span>
-              </li>
-              <li>
-                <GrMapLocation className="country-icon" /> Country:
-                <span>{country}</span>
-              </li>
-              <li>
-                <MdLocationCity className="city-icon" /> City:
-                <span>{city}</span>
-              </li>
-            </ul>
-          </div>
-          <Button
-            colorScheme={'red'}
-            onClick={() => {
-              handleDelete(id);
-              navigate('/goit-react-hw-08-phonebook/contacts');
-            }}
-          >
-            Delete contact
-          </Button>
         </div>
-      </div>
+      )}
     </>
   );
 };
