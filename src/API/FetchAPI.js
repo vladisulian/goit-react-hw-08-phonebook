611276efdec1a026a42a9aa769d05a7843ed4c32
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-03-05 02:52:36 +0200

    Edit styles in Registration.scss

--- src/API/FetchAPI.js
+++ src/API/FetchAPI.js
@@ -1,3 +1,4 @@
 export const TOKEN = `63fa063e473885d837d70d3e`;
 
-export const URL = `https://${TOKEN}.mockapi.io/contacts`;
+// export const URL = `https://${TOKEN}.mockapi.io/contacts`;
+export const URL = `https://connections-api.herokuapp.com`;
